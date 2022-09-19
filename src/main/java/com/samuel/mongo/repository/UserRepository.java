f4fa231734edc7969a37b54d94b849d4d31ff2d2
Samuel <samuel.batistela12@gmail.com>
2022-09-19 11:48:40 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/samuel/mongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.samuel.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.samuel.mongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+}
