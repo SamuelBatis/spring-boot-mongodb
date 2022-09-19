5a043357354a6d74495ff6baf56331ff30d2b4e6
Samuel <samuel.batistela12@gmail.com>
2022-09-19 18:47:18 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/samuel/mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.samuel.mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
