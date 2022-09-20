3267245e8568dbaa81bd3e803eb1feda88797ce7
Samuel <samuel.batistela12@gmail.com>
2022-09-20 16:42:51 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/samuel/mongo/domain/Post.java
+++ src/main/java/com/samuel/mongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.samuel.mongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.samuel.mongo.dto.AuthorDTO;
+import com.samuel.mongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -68,6 +73,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -89,6 +102,7 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
+
 	
 	
 }
